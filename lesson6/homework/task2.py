e9220e26af7cc1d10fa99a0ee9d2aaf12a0476b7
dmytroPPK <magnazer@gmail.com>
2023-04-30 22:13:41 +0300

    Homework #6 is done

---
+++ lesson6/homework/task2.py
@@ -0,0 +1,9 @@
+#2. Напишіть функцію square, яка приймає 1 аргумент, сторону квадрата, і повертає 3 значення
+# (за допомогою кортежу): периметр квадрата, площа квадрата та діагональ квадрата.
+def square(length):
+    perimetr = 4 * length
+    plosha = length ** 2
+    diagonal = (length ** 2 + length ** 2) ** 0.5
+    return perimetr, plosha, diagonal
+
+
---
+++ lesson6/homework/task6.py
@@ -0,0 +1,7 @@
+# 6.Напишіть функцію sum_range(start, end), яка підсумовує всі цілі числа від значення start до
+# величини end включно. Якщо користувач задасть перше число більше, ніж друге, просто поміняйте їх місцями.
+def sum_range(start, end):
+    if start > end:
+        start, end = end, start
+    list_to_sum = list(range(start, end + 1))
+    return sum(list_to_sum), list_to_sum
