45aadb97b587e45fb258393300cdc7dbec3a12e2
dmytroPPK <magnazer@gmail.com>
2023-07-23 20:54:25 +0300

    lesson23 is done

--- lesson23/conftest.py
+++ lesson23/conftest.py
@@ -9,13 +9,3 @@ def data_websites():
         'www.ithillel.ua'
     ]
     return data
-
-
-# @pytest.fixture
-# def data_websites():
-#     data = [
-#         'fake && ls bubu',
-#         'tfp.ukr.net',
-#         'wdn.ithillel.ua'
-#     ]
-#     return data
